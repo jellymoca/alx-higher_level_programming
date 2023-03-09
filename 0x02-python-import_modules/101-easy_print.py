9cf9d31f3eb09a9eed03644bca6c89a8f4b7d569
jellymoca <117826746+jellymoca@users.noreply.github.com>
2023-03-09 16:02:42 -0500

    Easy print

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Easy print of string"""
-import builtins
-builtins.print("#pythoniscool")
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
