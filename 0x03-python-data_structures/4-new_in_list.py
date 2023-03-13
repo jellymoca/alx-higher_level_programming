c99bb5803842baaecaa46fc1452beaec37422d61
jellymoca <117826746+jellymoca@users.noreply.github.com>
2023-03-13 14:22:15 -0400

    Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module contains a function that replaces an element
+in a list at a specific position.
+"""
+
+
+def new_in_list(my_list, idx, element):
+    """
+    Replace element in a list at a specific position
+    without modifying the original list.
+    """
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+    else:
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return new_list
