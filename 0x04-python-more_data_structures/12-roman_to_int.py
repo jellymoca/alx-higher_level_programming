997bd7df8f26527b90a873fc785a45e42f9fe515
jellymoca <117826746+jellymoca@users.noreply.github.com>
2023-03-15 14:58:00 -0400

    Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+
+    roman_dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    total = 0
+    n = len(roman_string)
+
+    for i in range(n):
+        if i + 1 < n and roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
+            total -= roman_dict[roman_string[i]]
+        else:
+            total += roman_dict[roman_string[i]]
+
+    return total
