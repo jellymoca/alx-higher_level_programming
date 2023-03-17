a190f18437fc4450258343190142c409ad84eb64
jellymoca <117826746+jellymoca@users.noreply.github.com>
2023-03-16 21:16:58 -0400

    CPython

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,59 +1,66 @@
-#include <stdio.h>
 #include <Python.h>
 
-/**
-* print_python_list - Prints list information
-*
-* @p: Python Object
-* Return: no return
-*/
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
 
+/**
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
+ */
 void print_python_list(PyObject *p)
 {
-Py_ssize_t size, alloc, i;
-PyObject *item;
+int size, alloc, i;
+const char *type;
+PyListObject *list = (PyListObject *)p;
+PyVarObject *var = (PyVarObject *)p;
 
-size = PyList_Size(p);
-alloc = ((PyListObject *)p)->allocated;
+size = var->ob_size;
+alloc = list->allocated;
 
 printf("[*] Python list info\n");
-printf("[*] Size of the Python List = %ld\n", size);
-printf("[*] Allocated = %ld\n", alloc);
+printf("[*] Size of the Python List = %d\n", size);
+printf("[*] Allocated = %d\n", alloc);
 
 for (i = 0; i < size; i++)
 {
-item = PyList_GetItem(p, i);
-printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+type = list->ob_item[i]->ob_type->tp_name;
+printf("Element %d: %s\n", i, type);
+if (strcmp(type, "bytes") == 0)
+print_python_bytes(list->ob_item[i]);
 }
 }
 
 /**
-* print_python_bytes - Prints bytes information
-*
-* @p: Python Object
-* Return: no return
-*/
-
+ * print_python_bytes - Prints basic info about Python byte objects.
+ * @p: A PyObject byte object.
+ */
 void print_python_bytes(PyObject *p)
 {
-Py_ssize_t size, i;
-char *s;
+unsigned char i, size;
+PyBytesObject *bytes = (PyBytesObject *)p;
 
 printf("[.] bytes object info\n");
-
-if (!PyBytes_Check(p))
+if (strcmp(p->ob_type->tp_name, "bytes") != 0)
 {
 printf("  [ERROR] Invalid Bytes Object\n");
 return;
 }
 
-size = PyBytes_Size(p);
-s = PyBytes_AsString(p);
+printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
+printf("  trying string: %s\n", bytes->ob_sval);
 
-printf("  size: %ld\n", size);
-printf("  trying string: %s\n", s);
+if (((PyVarObject *)p)->ob_size > 10)
+size = 10;
+else
+size = ((PyVarObject *)p)->ob_size + 1;
 
-printf("  first %ld bytes: ", (size < 10) ? size : 10);
-for (i = 0; i < ((size < 10) ? size : 10); i++)
-printf("%02x%c", (unsigned char)s[i], i == 9 ? '\n' : ' ');
+printf("  first %d bytes: ", size);
+for (i = 0; i < size; i++)
+{
+printf("%02hhx", bytes->ob_sval[i]);
+if (i == (size - 1))
+printf("\n");
+else
+printf(" ");
+}
 }
