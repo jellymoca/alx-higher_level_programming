47f84c1d4d97c8c224beb969711ed93d17b42db3
jellymoca <117826746+jellymoca@users.noreply.github.com>
2023-04-03 18:13:13 -0400

    Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3\
+#!/usr/bin/python3
 """Defines a Rectangle class."""
 
 
@@ -23,7 +23,7 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -36,10 +36,11 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        def area(self):
+
+    def area(self):
         """Return the area of the Rectangle."""
         return (self.__width * self.__height)
 
